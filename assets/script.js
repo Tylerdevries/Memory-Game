810b526243432ec2aaf9c8a4fe77ab0282c3f6e8
Tylerdevries <tylerdevries365@gmail.com>
2022-01-31 19:40:26 +0000

    added images

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+const section = document.querySelector("section");
+const playerLivesCount = document.querySelector("span");
+const playerLives = 8
+
+// Linking text
+playerLivesCount.textContent = playerLives;
+
